32a37006da428e9cba42a8a6a346170b41ec92c5
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-04-25 13:26:28 +0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
+//% weight=100 color=#C0392B icon=AB
+/**
+ * text(string)blocks.
+ */
+namespace text {
+    /**
+     * add text and use positions
+     */
+    //% block
+    export function text(text: string, font: number, x: number, y: number): void {
+        let textSprite = textsprite.create(text)
 
+        textSprite.setMaxFontHeight(font)
+
+        textSprite.setPosition(x, y)
+    }
+}
